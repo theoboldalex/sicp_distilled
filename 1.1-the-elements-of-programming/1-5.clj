24db5f4cc2ecee5b892a443330d4d47946e3e4b6
theoboldalex <theoboldalex@gmail.com>
2024-08-25 18:43:21 +0100

    completes 1.5

--- 1.1-the-elements-of-programming/1-5.clj
+++ 1.1-the-elements-of-programming/1-5.clj
@@ -6,18 +6,33 @@
 (defn p []
   (p))
 
-(defn p []
-  (p))
+(defn test [x y]
+  (if (= x 0)
+      0
+      y))
 
 ;; Then he evaluates the expression
 
-(test 0 (p))
+(test 0 (p)) ;; clojure  throws an unhandled java.lang.StackOverflowError
 
 ;; Q: What behavior will Ben observe with an interpreter that uses applicative-order evaluation?
-;; A:
+;; first we lookup the body of test
+(if (= x 0) 0 y)
+;; next we replace all values in place
+(if (= 0 0) 0 (p))
+;; A: p recurs infinitely until the stack overflows
 
 ;; Q: What behavior will he observe with an interpreter that uses normal-order evaluation?
-;; A:
+;; A: First, we lookup the body of test
+(if (= x 0) 0 y)
+;; Next we evaluate the operator before the operands
+(= x 0)
+;; we replace x with the argument applied
+(= 0 0)
+;; next, we evaluate the if condition which evaluates to true
+(= 0 0)
+;; x is zero so zero is returned
+0
 
 ;; (Assume that the evaluation rule for the special form if is the same whether the
 ;; interpreter is using normal or applicative order: The predicate expression is evaluated first,
